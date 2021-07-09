d4173290a4644fd15ac0903aa6d5f3911d1ffeef
veplotnikov <80313261+veplotnikov@users.noreply.github.com>
2021-07-09 23:15:43 +0700

    Update config.tf

--- config.tf
+++ config.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port        = 0
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
