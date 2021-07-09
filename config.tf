8a9ce7d5b8e530798b8db0ed196acc95f854aa55
veplotnikov <80313261+veplotnikov@users.noreply.github.com>
2021-07-09 21:17:05 +0700

    Create config.tf

---
+++ config.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.49.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  profile = "default"
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "build_server" {
+  ami           = "ami-05f7491af5eef733a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
