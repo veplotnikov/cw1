f21ab7f795361919ad7475930caa8ca30260924e
veplotnikov <80313261+veplotnikov@users.noreply.github.com>
2021-07-12 14:50:01 +0700

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,8 @@
+output "build" {
+description = "Public IP address of the EC2 instance"
+value       = aws_instance.build_server.public_ip
+  }
+output "build" {
+description = "Public IP address of the EC2 instance"
+value       = aws_instance.stage_server.public_ip
+  }
