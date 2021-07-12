b90d0fd5649614b4adcf4baad1ec4cd6041c5f45
veplotnikov <80313261+veplotnikov@users.noreply.github.com>
2021-07-12 23:32:51 +0700

    Update config.tf

--- config.tf
+++ config.tf
@@ -32,6 +32,14 @@ resource "aws_security_group" "allow_trafic" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description      = "http from VPC"
+    from_port        = 0
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
