07c634422346a84e7c05ec0e644eb4420aa74590
veplotnikov <80313261+veplotnikov@users.noreply.github.com>
2021-07-13 14:11:40 +0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "build" {
-description = "Public IP address of the EC2 instance"
+description = "Public IP address of the EC2 instance Build"
 value       = aws_instance.build_server.public_ip
   }
 output "stage" {
-description = "Public IP address of the EC2 instance"
+description = "Public IP address of the EC2 instance Stage"
 value       = aws_instance.stage_server.public_ip
   }
